f073a597dbfdc25fc64198edd2e9bb78cf6f3412
Marco Martinez <marcostaccato@gmail.com>
2024-07-04 20:53:42 -0600

    implement operations

--- src/main/java/com/staccato/challenge/core/Operations.java
+++ src/main/java/com/staccato/challenge/core/Operations.java
@@ -1,2 +1,44 @@
-package com.staccato.challenge.Core;public class Operations {
+package com.staccato.challenge.core;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class Operations {
+
+  public static int addition(int num1, int num2) {
+    return num1 + num2;
+  }
+
+  public static int substraction(int num1, int num2) {
+    return num1 - num2;
+  }
+
+  public static int multiplication(int num1, int num2) {
+    return num1 * num2;
+  }
+
+  //TODO: validate special cases
+  public static int division(int num1, int num2) {
+    return num1/num2;
+  }
+
+  public static double sqrt(int num1) {
+    return Math.sqrt(num1);
+  }
+
+  public static String random() {
+    String result = "";
+    try {
+      Document doc = Jsoup.connect("https://www.random.org/strings/?num=1&len=8&loweralpha=on&unique=on&format=plain&rnd=new").get();
+      Elements randomElements = doc.select("body");
+      result = randomElements.first().firstChild().toString().split("\n")[1];
+    } catch (IOException e) {
+      return "An error occurred, please try again";
+    }
+
+    return  result;
+  }
 }
--- src/test/java/com/staccato/challenge/core/OperationsTest.java
+++ src/test/java/com/staccato/challenge/core/OperationsTest.java
@@ -1,2 +1,68 @@
-package com.staccato.challenge.core;public class OperationsTest {
+package com.staccato.challenge.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OperationsTest {
+
+
+  @Test
+  public void addition_Test() {
+    Assertions.assertEquals(3, Operations.addition(1, 2));
+    Assertions.assertEquals(0, Operations.addition(0, 0));
+    Assertions.assertEquals(0, Operations.addition(1, -1));
+    Assertions.assertEquals(-2, Operations.addition(-1, -1));
+  }
+
+  @Test
+  public void substraction_Test() {
+    Assertions.assertEquals(1, Operations.substraction(2, 1));
+    Assertions.assertEquals(0, Operations.substraction(2, 2));
+    Assertions.assertEquals(2, Operations.substraction(1, -1));
+    Assertions.assertEquals(0, Operations.substraction(-1, -1));
+  }
+
+  @Test
+  public void multiplication_Test() {
+    Assertions.assertEquals(2, Operations.multiplication(2, 1));
+    Assertions.assertEquals(0, Operations.multiplication(0, 0));
+    Assertions.assertEquals(-1, Operations.multiplication(1, -1));
+    Assertions.assertEquals(1, Operations.multiplication(-1, -1));
+  }
+
+  @Test
+  public void division_Test() {
+    Assertions.assertEquals(1, Operations.division(2, 2));
+    Assertions.assertEquals(0, Operations.division(1, 2));
+    Assertions.assertEquals(-1, Operations.division(1, -1));
+    Assertions.assertEquals(1, Operations.division(-1, -1));
+  }
+
+  @Test
+  public void division_zero_by_zero_Test() {
+    Assertions.assertThrows(ArithmeticException.class, () -> Operations.division(0, 0));
+  }
+
+  @Test
+  public void division_by_zero_Test() {
+    Assertions.assertThrows(ArithmeticException.class, () -> Operations.division(1, 0));
+  }
+
+  @Test
+  public void sqrt_Test() {
+    Assertions.assertEquals(10, Operations.sqrt(100));
+    Assertions.assertEquals(0, Operations.sqrt(0));
+
+  }
+
+  @Test
+  public void random_Test() {
+    String result = Operations.random();
+    Assertions.assertNotNull(result);
+    Assertions.assertNotEquals("An error occurred, please try again", result);
+
+    String result2 = Operations.random();
+    Assertions.assertNotNull(result2);
+    Assertions.assertNotNull("An error occurred, please try again", result2);
+  }
 }
